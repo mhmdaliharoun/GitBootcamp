aeff7f93faca29263113dd762ab8fd3681ffdbec
Mohammad Ali Haroun <mohammadali.haroun@tagsoftwaregroup.com>
2026-07-10 12:34:33 +0300

    Lesson 7.4

--- tests/Get-Hello.Tests.ps1
+++ tests/Get-Hello.Tests.ps1
@@ -1,8 +1,21 @@
-# Dot-source the script to load its functions into the current scope
-. "$PSScriptRoot/../scripts/Get-Hello.ps1"
+Write-Host "PSScriptRoot = $PSScriptRoot"
+
+$ScriptPath = Join-Path $PSScriptRoot "../scripts/Get-Hello.ps1"
+
+Write-Host "Script Path = $ScriptPath"
+
+Test-Path $ScriptPath
+
+. $ScriptPath
+
+Get-Command Get-Hello
 
 Describe "Get-Hello" {
+
     It "Returns Hello" {
+
         Get-Hello | Should -Be "Hello"
+
     }
+
 }
