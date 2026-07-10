b1cb05ad4232a70f767bbf06760025d96a2898c3
Mohammad Ali Haroun <mohammadali.haroun@tagsoftwaregroup.com>
2026-07-10 12:21:14 +0300

    Lesson 7.3

--- tests/Get-Hello.Tests.ps1
+++ tests/Get-Hello.Tests.ps1
@@ -1,11 +1,8 @@
-Import-Module "$PSScriptRoot/../scripts/Get-Hello.ps1"
+# Dot-source the script to load its functions into the current scope
+. "$PSScriptRoot/../scripts/Get-Hello.ps1"
 
 Describe "Get-Hello" {
-
     It "Returns Hello" {
-
         Get-Hello | Should -Be "Hello"
-
     }
-
 }
